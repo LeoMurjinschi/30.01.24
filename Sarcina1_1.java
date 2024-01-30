9d9e29ad1220b08d4edb2a468386ff3132413a04
Murjinschi Leonard <leomurjinschi@gmail.com>
2024-01-30 17:48:06 +0200

    second commit

---
+++ Sarcina1_1.java
@@ -0,0 +1,9 @@
+public class Sarcina1_1
+{
+   public static void main(String[] args)
+   {
+	char litera='d';
+	if((litera=='a')||(litera=='e')||(litera=='i')||(litera=='o')||(litera=='u')) System.out.println("Vocala");
+	else System.out.println("Consoana");
+   }
+}
---
+++ Sarcina2_1.java
@@ -0,0 +1,14 @@
+public class Sarcina2_1
+{
+   public static void main(String[] args)
+   {
+	int n=120,k=1,s=0;
+	do
+	{
+	    s+=k;
+	    k++;
+	}
+	while(k<=n);
+	System.out.println(s);
+   }
+}
